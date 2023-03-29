5f7977be791048d7460ee2f3a6e678b0a16415d1
sc <sc@aivar.kr>
2023-03-29 17:55:16 +0900

    update

--- express-graphql1/index.js
+++ express-graphql1/index.js
@@ -1,5 +1,4 @@
 import express, { json } from 'express';
-import cors from 'cors';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 
@@ -18,7 +17,7 @@ const server = new ApolloServer({
 await server.start();
 
 // Specify the path where we'd like to mount our server
-app.use('/graphql', cors(), json(), expressMiddleware(server));
+app.use('/graphql', json(), expressMiddleware(server));
 
 const port = 3000;
 app.listen(port, () => {
